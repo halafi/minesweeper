74432806f1e82354c6c06f95be06b63ba3554adf
halafi <filiphalas74@gmail.com>
2020-05-12 20:12:30 +0800

    fix: difficulties

--- src/client/components/Minesweeper/consts/gameModes.ts
+++ src/client/components/Minesweeper/consts/gameModes.ts
@@ -13,16 +13,27 @@ const GAME_MODES = [
   },
   {
     name: 'Hard',
+    mines: 60,
+    width: 16, // 14 x 25
+    height: 16,
+  },
+  {
+    name: 'Very Hard',
     mines: 80,
     width: 18,
     height: 18,
   },
   {
-    name: 'Very Hard',
-    mines: 140,
-    width: 22,
-    height: 22,
+    name: 'Nightmare',
+    mines: 120,
+    width: 20,
+    height: 20,
   },
 ];
 
 export default GAME_MODES;
+// 10 x 10 / 10 = 10
+// 14 x 14 / 30 = 6,53
+// 16 x 16 / 60 = 4,27
+// 18 x 18 / 80 = 4,05
+// 20 x 20 / 120 = 3,33
