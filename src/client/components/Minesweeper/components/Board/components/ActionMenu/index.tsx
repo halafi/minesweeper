251441bcb3f519c88d2c869dc5fb6314db9170e7
halafi <filiphalas74@gmail.com>
2020-05-13 20:25:30 +0800

    improvement: mobile

--- src/client/components/Minesweeper/components/Board/components/ActionMenu/index.tsx
+++ src/client/components/Minesweeper/components/Board/components/ActionMenu/index.tsx
@@ -14,8 +14,8 @@ const Root = styled.div`
   height: 80px;
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  box-shadow: 0 -2px 3px 0 rgba(0, 0, 0, 0.5);
+  justify-content: flex-end;
+  box-shadow: 0 -3px 3px 0 rgba(0, 0, 0, 0.5);
   max-width: 500px;
   ${media.tablet} {
     max-width: 600px;
@@ -27,20 +27,28 @@ const MenuItem = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 0 20px;
+  :first-child {
+    margin: 0 10px;
+  }
+  :last-child {
+    margin: 0 20px 0 10px;
+  }
 `;
 
 const Image = styled.img`
   width: 32px;
 `;
 
-const ButtonWrapper = styled.div`
+type ButtonWrapperProps = {
+  disabled: boolean;
+};
+const ButtonWrapper = styled.div<ButtonWrapperProps>`
   display: flex;
   align-items: center;
   justify-content: center;
   cursor: pointer;
   border-radius: 50%;
-  background: rgba(0, 0, 0, 0.7);
+  background: ${({ disabled }) => (disabled ? 'rgba(0, 0, 0, 0.4)' : 'rgba(0, 0, 0, 0.7)')};
   padding: 12px;
 `;
 
@@ -52,20 +60,16 @@ type Props = {
 
 const ActionMenu = ({ onDig, onFlag, enabled }: Props) => (
   <Root>
-    {enabled && (
-      <>
-        <MenuItem>
-          <ButtonWrapper onClick={onFlag}>
-            <Image src="/images/sign.png" alt="warning sign" />
-          </ButtonWrapper>
-        </MenuItem>
-        <MenuItem>
-          <ButtonWrapper onClick={onDig}>
-            <Image src="/images/shovel.png" alt="dig" />
-          </ButtonWrapper>
-        </MenuItem>
-      </>
-    )}
+    <MenuItem>
+      <ButtonWrapper onClick={enabled ? onFlag : undefined} disabled={!enabled}>
+        <Image src="/images/sign.png" alt="warning sign" />
+      </ButtonWrapper>
+    </MenuItem>
+    <MenuItem>
+      <ButtonWrapper onClick={enabled ? onDig : undefined} disabled={!enabled}>
+        <Image src="/images/shovel.png" alt="dig" />
+      </ButtonWrapper>
+    </MenuItem>
   </Root>
 );
 
